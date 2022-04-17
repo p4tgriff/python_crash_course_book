2aec1dc4156706f70eb38be4599dd9a4c0a609be
Patrick Griffith <bbyguy@gmail.com>
2022-04-17 12:16:51 -0500

    Init commit - Python_Crash_Course

---
+++ Chapter_3/motorcycles copy.py
@@ -0,0 +1,19 @@
+# motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+# print(motorcyles)
+
+
+# Organizing a list
+
+# cars = ['bmw', 'audi', 'toyota', 'honda', 'ford']
+# # cars.sort(reverse=True)
+# # "reverse=True changes the a-z direction of the sort"
+# # print(cars)
+
+# print("Here is the original list:")
+# print(cars)
+
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
+
+# print("\nHere is the original list again:")
+# print(cars)

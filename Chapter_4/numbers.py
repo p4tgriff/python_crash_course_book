90501a0192cadde4ca8dc0d6fee2e15de4421cda
Patrick Griffith <bbyguy@gmail.com>
2022-04-19 22:01:25 -0500

    added numbers.py

---
+++ Chapter_4/numbers.py
@@ -0,0 +1,11 @@
+for value in range(1, 5):
+    print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+odd_numbers = list(range(1, 7, 2))
+print(odd_numbers)

13a5daaf5cb2e6a0716ed2c0799ef9e9c64204a9
Patrick Griffith <bbyguy@gmail.com>
2022-04-24 18:49:47 -0500

    added tuples.py

---
+++ Chapter_4/tuples.py
@@ -0,0 +1,10 @@
+foods = ('pbj', 'mac&cheese', 'potato salad', 'red beans', 'grilled cheese')
+for food in foods:
+    print(food)
+
+# foods[0] = 'pizza'
+
+foods = ('hamburger', 'mac&cheese', 'potato salad',
+         'red beans', 'grilled cheese')
+for food in foods:
+    print(food)

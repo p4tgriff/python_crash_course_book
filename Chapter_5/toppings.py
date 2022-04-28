14c185dbc68d36f6da30acbcce4b261cecb1e07e
Patrick Griffith <bbyguy@gmail.com>
2022-04-27 21:47:36 -0500

    added to toppings.py book example

--- Chapter_5/toppings.py
+++ Chapter_5/toppings.py
@@ -6,3 +6,17 @@ if requested_topping != 'anchovies':
 
 requested_toppings = ['mushrooms', 'onions', 'pineapple']
 'mushrooms' in requested_toppings
+
+
+availabale_toppings = ['mushrooms', 'olives',
+                       'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in availabale_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+
+print("\nFinished making your pizza!")

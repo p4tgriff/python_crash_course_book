e15a21ddf2fa34d964d0bba7b9050ecf57d1ea2d
Patrick Griffith <bbyguy@gmail.com>
2022-05-08 21:39:42 -0500

    added keys and values to rivers.py

--- Chapter_6/rivers.py
+++ Chapter_6/rivers.py
@@ -9,3 +9,9 @@ print(rivers)
 for river in rivers:
     creek = rivers[river].title()
     print(f"\n{river.title()} is the biggest river in {creek}.")
+
+for x in rivers.keys():
+    print(x)
+
+for y in rivers.values():
+    print(y)

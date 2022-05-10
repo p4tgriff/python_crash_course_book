032699e5b337f2683a9bcd0257b23aada230dca1
Patrick Griffith <bbyguy@gmail.com>
2022-05-09 20:08:59 -0500

    added alien1,2,3 to dictionary

--- Chapter_6/aliens.py
+++ Chapter_6/aliens.py
@@ -34,5 +34,14 @@ print(f"New position: {alien_0['x_position']}.")
 
 # Remove from the dictionary using 'del'
 
-del alien_0['points']
-print(alien_0)
+# del alien_0['points']
+# print(alien_0)
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'red', 'points': 10}
+alien_2 = {'color': 'yellow', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)

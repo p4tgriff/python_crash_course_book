02c39199cf29d0040768871123ac2957aa2ddcac
Patrick Griffith <bbyguy@gmail.com>
2022-05-09 20:54:07 -0500

    changes to pets.py

---
+++ Chapter_6/pets.py
@@ -0,0 +1,31 @@
+pets = ['dog_0', 'dog_1', 'dog_2', 'cat']
+
+dog_0 = {
+    'name': 'Lucky',
+    'owner': 'Tim',
+    'age': '3'
+}
+
+dog_1 = {
+    'name': 'Jolie',
+    'owner': 'Sara',
+    'age': '8'
+}
+
+dog_2 = {
+    'name': 'Spot',
+    'owner': 'Hank',
+    'age': '6'
+}
+
+cat = {
+    'name': 'Faith',
+    'owner': 'Lauren',
+    'age': '13'
+}
+
+# pets = ['dog_0', 'dog_1', 'dog_2', 'cat']
+
+for animals, pet in pets.items():
+    print(f"My name is {pet['name']}, my owner's name is {pet['owner']}.")
+    print(f"I am {pet['age']} years old.")

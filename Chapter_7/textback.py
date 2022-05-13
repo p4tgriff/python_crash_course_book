d3f0b776c7275ed8001e846f760d6b7defa9cdd6
Patrick Griffith <bbyguy@gmail.com>
2022-05-12 21:30:08 -0500

    added dinner table question

--- Chapter_7/textback.py
+++ Chapter_7/textback.py
@@ -8,3 +8,12 @@ if car == 'Toyota':
     print("Yes, we have a Toyota for you.")
 else:
     print("Sorry, we only have a Toyota for you today!")
+
+
+table = input("How many people are in your dinner party? ")
+table = int(table)
+
+if table >= 8:
+    print("Due to your party size, there will be a 15 minute wait.")
+else:
+    print("Your table is ready now.")

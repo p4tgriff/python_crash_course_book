186ab40a43298d2f96a3658971edde34b0b1c940
Patrick Griffith <bbyguy@gmail.com>
2022-05-12 21:27:35 -0500

    car questionaire added

--- Chapter_7/textback.py
+++ Chapter_7/textback.py
@@ -1,2 +1,10 @@
-message = input("Tell me something and I'll repeat it back to you: ")
-print(message)
+# message = input("Tell me something and I'll repeat it back to you: ")
+# print(message)
+
+car = input("What kind of rental car would you like today? ")
+car = car
+
+if car == 'Toyota':
+    print("Yes, we have a Toyota for you.")
+else:
+    print("Sorry, we only have a Toyota for you today!")

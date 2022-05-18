47d4b78fa13184d6142f130ded0fd27a31776d61
Patrick Griffith <bbyguy@gmail.com>
2022-05-18 16:38:19 -0500

    added break to loop

--- Chapter_7/parrot.py
+++ Chapter_7/parrot.py
@@ -1,6 +1,13 @@
 prompt = "\nTell me something, and I will repeat it back to you."
 prompt += "\nEnter 'quit' to end the program."
 
+
+# while message != 'quit':
+#     message = input(prompt)
+
+#     if message != 'quit':
+#         print(message)
+
 active = True
 while active:
     message = input(prompt)
@@ -10,8 +17,15 @@ while active:
     else:
         print(message)
 
-# while message != 'quit':
-#     message = input(prompt)
+# using break to exit a loop
 
-#     if message != 'quit':
-#         print(message)
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.)"
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")

609e90196b3c26d354cacfdb1fcf4977ee264af7
Patrick Griffith <bbyguy@gmail.com>
2022-05-18 16:18:52 -0500

    updated quit message in parrot.py

--- Chapter_7/parrot.py
+++ Chapter_7/parrot.py
@@ -4,4 +4,6 @@ prompt += "\nEnter 'quit' to end the program."
 message = ""
 while message != 'quit':
     message = input(prompt)
-    print(message)
+
+    if message != 'quit':
+        print(message)

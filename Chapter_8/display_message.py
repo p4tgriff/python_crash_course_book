8dfc61063d3d819ba98ef821d2c0236de9e3baf6
Patrick Griffith <bbyguy@gmail.com>
2022-05-31 22:30:49 -0500

    added a message for my favorite book

---
+++ Chapter_8/display_message.py
@@ -0,0 +1,15 @@
+def display_message(name):
+    """Display a message telling what you're learning."""
+    print(
+        f"\nI am learning how functions work in chapter 8. {name.title()}, will you learn with me?")
+
+
+display_message('Brittni')
+
+
+def favorite_book(book):
+    """prints a message regarding my favorite book."""
+    print(f"\nOne of my favorite books is '{book.title()}'.")
+
+
+favorite_book('How to win friends and influence people.')

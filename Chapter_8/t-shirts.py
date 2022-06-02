630ed05eb03820ddc3a14b14be5a4195fbeacd32
Patrick Griffith <bbyguy@gmail.com>
2022-06-01 22:17:38 -0500

    added t-shirt.py

---
+++ Chapter_8/t-shirts.py
@@ -0,0 +1,8 @@
+def make_shirt(shirt_size, shirt_phrase):
+    """asasd"""
+    print(f"\nI will need a size {shirt_size} shirt.")
+    print(f"On the shirt I want it to say, '{shirt_phrase}'.")
+
+
+make_shirt('large', 'I love dogs')
+make_shirt(shirt_phrase='Dogs are awesome', shirt_size='medium')

8f156a2bc4a24d230d3d08c8f181103b311625fa
Patrick Griffith <bbyguy@gmail.com>
2022-06-01 22:13:11 -0500

    testing multiple outputs for pets.py

--- Chapter_8/pets.py
+++ Chapter_8/pets.py
@@ -1,7 +1,11 @@
 def describe_pets(animal_type, pet_name):
     """Display information about a pet."""
     print(f"\nI have a {animal_type}.")
-    print(f"\nMy {animal_type}'s name is {pet_name}.")
+    print(f"My {animal_type}'s name is {pet_name}.")
 
 
 describe_pets('hamster', 'Harry')
+describe_pets('Belgian Malinois', 'Stella')
+describe_pets(pet_name='Ryder', animal_type='pound puppy')
+# describe_pets('Ryder', animal_type='pound puppy')
+# describe_pets(pet_name='Ryder', 'pound puppy')

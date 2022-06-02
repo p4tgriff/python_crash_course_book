8125765502ac0a8c9bc3818b7f4a625dcef6d038
Patrick Griffith <bbyguy@gmail.com>
2022-06-01 20:25:43 -0500

    added pets.py

---
+++ Chapter_8/pets.py
@@ -0,0 +1,7 @@
+def describe_pets(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"\nMy {animal_type}'s name is {pet_name}.")
+
+
+describe_pets('hamster', 'Harry')

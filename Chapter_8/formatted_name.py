febe3ae6c7748dd66b0185cf3ba3f4d74e820a5f
Patrick Griffith <bbyguy@gmail.com>
2022-06-16 20:35:26 -0500

    added Return a dictionary to formatted_name.py

--- Chapter_8/formatted_name.py
+++ Chapter_8/formatted_name.py
@@ -6,3 +6,15 @@ def get_formatted_name(first_name, last_name):
 
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
+
+
+def build_person(first_name, last_name, age=None):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+
+musician = build_person('Jimi', 'Hendrix', age=27)
+print(musician)

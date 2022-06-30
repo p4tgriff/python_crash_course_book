0eb194249d68104372fe7405e6f8ce19356c010a
Patrick Griffith <bbyguy@gmail.com>
2022-06-29 22:11:56 -0500

    commented out pizza.py  added making_pizza.py

--- Chapter_8/pizza.py
+++ Chapter_8/pizza.py
@@ -1,21 +1,21 @@
-def make_pizza(*toppings):
-    """Print the list of toppings that have been requested."""
-    print(toppings)
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested."""
+#     print(toppings)
 
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 
-def make_pizza(*toppings):
-    """Summarize the pizza we are about to make."""
-    print("\nMaking a pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
+# def make_pizza(*toppings):
+#     """Summarize the pizza we are about to make."""
+#     print("\nMaking a pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
 
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 
 def make_pizza(size, *toppings):
@@ -25,5 +25,5 @@ def make_pizza(size, *toppings):
         print(f"- {topping}")
 
 
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+# make_pizza(16, 'pepperoni')
+# make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')

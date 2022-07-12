504404bcb71aadb4b00cb5d20fa86f4b50207f06
Patrick Griffith <bbyguy@gmail.com>
2022-07-11 22:24:18 -0500

    added customer data to restuarant.py

--- Chapter_9/restaurant.py
+++ Chapter_9/restaurant.py
@@ -4,6 +4,7 @@ class Restaurant:
     def __init__(self, name, type):
         self.name = name
         self.type = type
+        self.customer_number = 0
 
     def describe_restaurant(self):
         print(
@@ -11,6 +12,16 @@ class Restaurant:
 
     def open_restaurant(self):
         print(f'{self.name} is open for business.')
+
+    def current_customer(self):
+        print(f'Now serving customer number {self.customer_number}.')
+
+    def set_number_served(self, number):
+        self.customer_number = number
+        print(f'Now serivce custmer {self.number}!')
+
+    def increment_number_server(self,):
+        print(f'')
 
 
 my_restaurant = Restaurant('Johnnys', 'pizza')
@@ -20,3 +31,6 @@ print(f'\nThey are a {my_restaurant.type} place.')
 
 my_restaurant.describe_restaurant()
 my_restaurant.open_restaurant()
+
+my_restaurant.customer_number = 3
+my_restaurant.current_customer()

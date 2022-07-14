87cecea999020d9edfbc1879cf84668e1255dc6f
Patrick Griffith <bbyguy@gmail.com>
2022-07-13 22:54:59 -0500

    adding battery attribute and methods

--- Chapter_9/electric_car.py
+++ Chapter_9/electric_car.py
@@ -24,12 +24,23 @@ class Car:
         self.odometer_reading += miles
 
 
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=75):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(f'This car has a {self.battery_size}-kWh battery.')
+
+
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles."""
 
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
+        self.battery = Battery()
 
 
 my_tesla = ElectricCar('telsa', 'model s', 2019)

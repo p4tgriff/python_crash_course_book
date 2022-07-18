59766ce2fc0d6ab6bb787d106607dc53140ebc60
Patrick Griffith <bbyguy@gmail.com>
2022-07-17 19:40:18 -0500

    added a get_range method to battery class

--- Chapter_9/electric_car.py
+++ Chapter_9/electric_car.py
@@ -33,6 +33,15 @@ class Battery:
     def describe_battery(self):
         print(f'This car has a {self.battery_size}-kWh battery.')
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 135
+
+        print(f'This car can go about {range} miles on a full charge.')
+
 
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles."""
@@ -47,3 +56,4 @@ my_tesla = ElectricCar('telsa', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
 
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()

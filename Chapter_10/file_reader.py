b4e5a89e65c10c65b57160f299b361930a2aa1bd
Patrick Griffith <bbyguy@gmail.com>
2022-07-20 21:02:40 -0500

    added code to replace .txt wording

--- Chapter_10/file_reader.py
+++ Chapter_10/file_reader.py
@@ -7,3 +7,20 @@ filename = 'py_digits.txt'
 with open(filename) as file_object:
     for line in file_object:
         print(line.rstrip())
+
+with open('learning_python.txt') as file_object:
+    contents = file_object.read()
+print(contents)
+
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+
+filename = 'learning_python.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.replace('Python', 'C'))

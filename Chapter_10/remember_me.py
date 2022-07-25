d3614100dceaa4e686c92c4d40adf30aba03d870
Patrick Griffith <bbyguy@gmail.com>
2022-07-24 21:57:47 -0500

    split into 2 different functions

--- Chapter_10/remember_me.py
+++ Chapter_10/remember_me.py
@@ -4,21 +4,31 @@ import json
 # Otherwise, prompt for the username and store it.
 
 
-def greet_user():
-    """Greet the user by name."""
+def get_stored_username():
+    """Get stored username if available"""
 
     filename = 'username.json'
-
     try:
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
-        username = input("What is your name?")
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"We'll remmeber you when you come back, {username}!")
+        return None
     else:
-        print(f"Welcome back {username}!")
+        return username
+
+
+def greet_user():
+    """Greet the user by name."""
+
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        usernanme = input("What is your name?")
+        filename = 'username.json'
+        with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"We'll remmeber you when you come back, {username}!")
 
 
 greet_user()
